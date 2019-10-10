37108c0dd3ddd12d580bd4900434408a7046ab35
Pranav <pranav@localhost>
2019-10-09 21:15:23 -0700

    Initial check-in of assignment2

---
+++ CSE_360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,38 @@
+/*
+Pranav Suresh
+Class ID: CSE 360 - 70642
+Assignment Number: 2
+Description: This code's eventual intention seems to be to 
+               add and subtract numbers to the 'total' variable.
+*/
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total; //variable to add to and subtract from
+	
+	public AddingMachine () {
+		total = 0; //not needed - included for clarity
+	}
+
+	public int getTotal() {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+	}
+	
+	public void clear() {
+		
+	}
+}
